f23a27d27b985921e33ea5409e18a41a71d45dfa
Cody_Jones <cbjones93@gmail.com>
2021-06-24 12:07:33 -0500

    working on walksRepository

---
+++ DogGo/Repositories/WalksRepository.cs
@@ -0,0 +1,12 @@
+﻿using DogGo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo.Repositories
+{
+    public class WalksRepository
+    {
+    }
+}
